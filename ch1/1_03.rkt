4ba4818d305dec0ca21c947fc89ccac2ce9e73e9
Ryan Troxler <rtroxler@rednovalabs.com>
2016-12-07 22:45:28 -0600

    lispier solution

--- ch1/1_03.rkt
+++ ch1/1_03.rkt
@@ -1,12 +1,13 @@
 #lang racket
 
 (define (big-ol-sums x y z)
-  (cond ((and (< x y) (< x z)) (+ (square y) (square z)))
-        ((and (< y x) (< y z) (+ (square x) (square z))))
-        (else (+ (square x) (square y)))))
+  (+ (square (car (two-largest x y z)))
+     (square (cadr (two-largest x y z)))))
 
 (define (square x) (* x x))
 
+(define (two-largest . nums)
+    (list-tail (sort nums <) (- (length nums) 2)))
 
 (if (= (big-ol-sums 1 2 3) 13)
   (println "success")
