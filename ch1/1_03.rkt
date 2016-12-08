68ea7bc8c7e75770e5c08a2ae7471d2f04af4af3
Ryan Troxler <rtroxler@rednovalabs.com>
2016-12-07 22:18:32 -0600

    learn to read

--- ch1/1_03.rkt
+++ ch1/1_03.rkt
@@ -1,19 +1,21 @@
 #lang racket
 
 (define (big-ol-sums x y z)
-  (cond ((and (< x y) (< x z)) (+ y z))
-        ((and (< y x) (< y z) (+ x z)))
-        (else (+ x y))))
+  (cond ((and (< x y) (< x z)) (+ (square y) (square z)))
+        ((and (< y x) (< y z) (+ (square x) (square z))))
+        (else (+ (square x) (square y)))))
+
+(define (square x) (* x x))
 
 
-(if (= (big-ol-sums 1 2 3) 5)
+(if (= (big-ol-sums 1 2 3) 13)
   (println "success")
   (println "fail"))
 
-(if (= (big-ol-sums 3 1 2) 5)
+(if (= (big-ol-sums 3 1 2) 13)
   (println "success")
   (println "fail"))
 
-(if (= (big-ol-sums 3 2 1) 5)
+(if (= (big-ol-sums 3 2 1) 13)
   (println "success")
   (println "fail"))
