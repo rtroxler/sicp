7a2e17f24f67d4b66fa99608014402f23398ff99
Ryan Troxler <rtroxler@rednovalabs.com>
2016-12-12 20:22:50 -0600

    1.8

---
+++ ch1/1_08.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+
+(define (cube-root x)
+  (cube-iter 1.0 x))
+
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (cube-iter (improve guess x) x)))
+
+(define (improve guess x)
+  (/
+    (+ (/ x (square guess)) (* 2 guess))
+    3))
+
+(define (good-enough? guess x)
+  (< (abs (- (cube guess) x)) 0.001))
+
+(define (cube x)
+  (* x x x))
+
+(define (square x)
+  (* x x))
+
+
+(cube 3)
+
+(cube-root 27)
+
+(cube-root 81)
