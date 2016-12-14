061262638cec398c7119289a4886b26401892ced
Ryan Troxler <rtroxler@rednovalabs.com>
2016-12-13 22:11:41 -0600

    Update 1.8 to add lexical scoping

--- ch1/1_08.rkt
+++ ch1/1_08.rkt
@@ -2,27 +2,26 @@
 
 
 (define (cube-root x)
-  (cube-iter 1.0 x))
+  (define (cube x)
+    (* x x x))
+  (define (good-enough? guess)
+    (< (abs (- (cube guess) x)) 0.001))
+  (define (improve guess)
+    (/
+      (+ (/ x (square guess)) (* 2 guess))
+      3))
+  (define (cube-iter guess)
+    (if (good-enough? guess)
+      guess
+      (cube-iter (improve guess))))
+  (cube-iter 1.0))
 
-(define (cube-iter guess x)
-  (if (good-enough? guess x)
-    guess
-    (cube-iter (improve guess x) x)))
-
-(define (improve guess x)
-  (/
-    (+ (/ x (square guess)) (* 2 guess))
-    3))
-
-(define (good-enough? guess x)
-  (< (abs (- (cube guess) x)) 0.001))
 
 (define (cube x)
   (* x x x))
 
 (define (square x)
   (* x x))
-
 
 (cube 3)
 
