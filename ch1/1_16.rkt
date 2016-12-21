cb7f7fca5f02067242176613fd20a4a1d55065eb
Ryan Troxler <rtroxler@rednovalabs.com>
2016-12-21 08:30:13 -0600

    1.16

---
+++ ch1/1_16.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+; ab^n is unchanged for each iteration
+
+(define (expt b n)
+  (a-fast-expt b n 1))
+
+(define (a-fast-expt b n a)
+  (cond ((= n 0) a)
+        ((even? n)
+         (a-fast-expt (square b) (/ n 2) a))
+        (else
+          (a-fast-expt b (- n 1) (* a b)))))
+
+(define (square x) (* x x))
+
+(expt 11 45)
+; shit's fast
+
